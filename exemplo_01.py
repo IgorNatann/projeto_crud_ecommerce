2cf0396afe5b23772f9dc333926219d92d4e8dbb
IgorNatann <Igornatan4@gmail.com>
2026-04-09 17:31:37 -0300

    feat/adiciona_db:efetua conexao e cria tabela.

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,8 +1,38 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Cria Conexão com o SQLlite em Memória
-URI = 'sqlite:///:sqllite_db:'
+URI = 'sqlite:///data/database_exemplo_1.db'
 
 engine = create_engine(URI, echo=True)
 
 print('Conexão estabelecida com sucesso!')
+
+#ORM - Object Relational Mapping
+Base = declarative_base()
+
+# Herdado da classe Base para criar a tabela de usuários
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, nullable=False)
+    idade = Column(Integer, nullable=False)
+
+Base.metadata.create_all(engine)
+
+print("Tabela Criada com SQLite estabelecida.") 
+
+#  Criando uma sessão para interagir com o banco de dados, efetua insert utilizando commit e encerra.
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_usuario = Usuario(nome='João', idade=28)
+session.add(novo_usuario)
+session.commit()
+
+print("Usuário inserido com sucesso.")
+
+usuario = session.query(Usuario).filter_by(nome='João').first()
+print(f"Usuário encontrado: {usuario.nome}, Idade: {usuario.idade}")
